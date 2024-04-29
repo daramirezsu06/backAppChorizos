c716724bab8a020952a0fc6612029c087750a71a
daramirezsu06 <daramirezsu06@gmail.com>
2024-04-29 09:25:21 -0500

    pruebas deply2

--- dist/routes/costumersRoutes.js
+++ dist/routes/costumersRoutes.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const costumerControllers_1 = require("../controllers/costumerControllers");
 const costumerRoutes = (0, express_1.Router)();
-costumerRoutes.post("/", costumerControllers_1.createCostumer);
+costumerRoutes.get("/", costumerControllers_1.createCostumer);
 exports.default = costumerRoutes;
